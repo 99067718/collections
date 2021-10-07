bd01a8f7c1ced0f20190495e0aa9a01b45baaca8
Emiel 't Lam <99067718@mydavinci.nl>
2021-10-07 13:51:26 +0200

    Aantal snoepies

--- M&M.py
+++ M&M.py
@@ -1,14 +1,34 @@
 import random
-
+Zak = []
+Zak_V2 = {}
 
 colors = ["oranje", "blauw", "groen", "bruin"]
-Zak = []
 
+def amount():
+    return int(input("Hoeveel M&M's wil je in je zakje?: "))
 
-def toevoegen(kleuren:list, aantal: int):
+def listToStringWithoutBrackets(list1):
+    return str(list1).replace('[','').replace(']','').replace(",",'')
+
+def toevoegen(aantal: int):
     for i in range(aantal):
-        Zak.append(random.sample(kleuren,1))
+        new = random.choice(colors)
+        Zak.append(new)
     return Zak
 
-toevoegen(colors,10)
-print(Zak)
+def diction(aantal: int):
+    for i in range(aantal):
+        new = random.choice(colors)
+        if new in Zak_V2:
+            Zak_V2[new] += 1
+        else:
+            Zak_V2[new] = 1
+    return Zak_V2
+        
+
+toevoegen(amount())
+
+print(listToStringWithoutBrackets(Zak))
+
+print(diction(amount()))
+
